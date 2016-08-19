bab5f400d10c2be8fe632d69fc4d89678ec8c91e
jliu4 <jliuapril@gmail.com>
2016-08-19 16:18:08 -0700

    diff loop

--- matlab/LoopQvQhTemp.m
+++ matlab/LoopQvQhTemp.m
@@ -5,7 +5,7 @@ addpath('C:\jinwork\BE\matlab\addaxis5')
 
 %looking at Berkeley HHT
 SYS = 'BEC Core B37';
-whichEx = 2;
+whichEx = 3;
 Directory='C:\jinwork\BEC\data\ConF00_copy\2016-07-21';
 AllFiles = getall(Directory);  %SORTED BY DATE....
 
@@ -16,9 +16,13 @@ AllFiles = getall(Directory);  %SORTED BY DATE....
 %5 25 (8/14/2016 6:00 - 8/15/2016 5:59)
 %6 26 (8/15/2016 6:00 - 8/16/2016 5:59)
 %7 27 (8/16/2016 6:00 - 8/17/2016 5:59)
+%8 28 (8/17/2016 6:00 - 8/18/2016 5:59)
+%9 29 (8/18/2016 6:00 - 8/19/2016 5:59)
+%10 30 (8/18/2016 23:00 - 8/19/2016 13:00)
 %e1 8/10/2016 16:35(10 hours) - 8/12/2016 9:35 (20 hours) 
 %e2 8/12/2016 17:00(11 hours) - 8/15/2016 17:45 (11 hours)
-%e3 8/15/2016 22:00(16 hours)
+%e3 8/15/2016 22:00(16 hours) - 8/18/2016 20:00 (10 hours)
+%e4 8/18/2016 23:00(17 hours) - 8/19/2016 13:00 (17 hours)
 switch (whichEx)
     case 1 
         startTime = 10; %11 hours after 6:00
@@ -34,18 +38,21 @@ switch (whichEx)
         columnPerFigure = 3;
 
     case 3
-        startTime = 16; %
-        endTime = 0;
-        Experiment = AllFiles(6:7);
+        startTime = 13; %
+        endTime = 5;
+        Experiment = AllFiles(6:9);
         rowPerFigure = 5;
         columnPerFigure = 3;
-
+   case 4
+        startTime = 10; %
+        endTime = 0;
+        Experiment = AllFiles(9:10);
+        rowPerFigure = 1;
+        columnPerFigure = 1;
 
     otherwise
         exit
 end;
-
-%Experiment = AllFiles(4:7);
 Experiment'
 
 loadHHT 
@@ -66,19 +73,9 @@ qi=find(QOccurred == 1);
 size(DateTime);
 j1 = horzcat(dateN,InnerCoreTemp,CoreHtrPow,QkHz,QPulseVolt);
 
-size(j1);
 %start only when runs started
 j1=j1(startTime*360:end-endTime*360,:);
-%take out temperature noise
-%IC1 = j1(:,2) < 90 ;
-%tabulate(IC1)
-%j1(IC1,:)=[];
 
-%IC2 = j1(:,2) > 610;
-%tabulate(IC2)
-%j1(IC2,:)=[];
-%size(j1)
-t1 = [100 200 300 400 500 600];
 tp = [];
 vi = 0;
 for qV = 50:50:250
@@ -93,7 +90,7 @@ for qV = 50:50:250
         j2 = j1((abs(j1(:,4)-qkHz) < 1 & abs(j1(:,5)-qVC) < 19) ,:);
         
         if size(j2(:,1)) > 0
-            fn = ['C:\jinwork\BEC\data\q' num2str(whichEx) num2str(qV) num2str(qkHz) '.csv'];
+            fn = ['C:\jinwork\BEC\tmp\q' num2str(whichEx) num2str(qV) num2str(qkHz) '.csv'];
             
             dt = datetime(j2(:,1), 'ConvertFrom', 'datenum') ;
             
@@ -101,12 +98,7 @@ for qV = 50:50:250
             
             T = table(dt,j2(:,2),j2(:,3),j2(:,4),j2(:,5),'VariableName',{'DateTime','InnerCoreTemp','Power','QkHz','Qvolts'});
             writetable(T,fn);
-            %fileID = fopen(fn,'w');
-            %fprintf(fileID,
-            %fprintf(fileID,'%datetime %6.2f %6.2f %6.2f %6.2f %6.2f',dt,j2(:,2),j2(:,3),j2(:,4),j2(:,5));
-            %fclose(fileID);
-            %dlmwrite(fn1, j2,',');
-            %dlmwrite(fn1, dt);
+            if (1==0)
             figure(1)
             grid
             subplot(rowPerFigure,columnPerFigure,ki + (vi-1)*3)
@@ -127,49 +119,55 @@ for qV = 50:50:250
             %plot(dt,j2(:,3),'linewidth',2)
             plot(dt,j2(:,3))
             hold off
+            end
             j4 = [];
+            i = 0
             for temp = [100 200 300 400 500 600]
+                i = i+1
                 %pick up data with the particular tempareture
-                j3 = j2((abs(j2(:,2)-temp) < 15) ,:);
-                dt3 = datetime(j3(:,1), 'ConvertFrom', 'datenum') ;
-                
-                if size(j3(:,1)) > 0   
-                   if dt3 
-                    j4(temp/100) = mean(j3(end-15:end-5,3));
+                j3 = j2((abs(j2(:,2)-temp) < 5) ,:);
+                nj3 = size(j3(:,1),1)
+                j5(i) = nj3   
+                j4(i) = 0
+                t2(i) = temp
+                if nj3 > 20 % minimum requirement is 40 minutes for power watt change < 0.3  
+                    for ni = nj3:-1:2 
+                        %search the core hrt power change less than 0.3 W
+                        j3(ni,3)
+                        if (abs(j3(ni,3)-j3(ni-5,3))<=0.3 & j3(ni,3) > 0 & j3(ni,3) < 150)
+                            
+                            j4(i) = j3(ni,3)
+                            
+                           break;
+                        end
+                    end    
                 end
             end
-            nt = size(j4,2);
-            if nt > 0 
-                t2 = t1; %(1:nt);
-                p=polyfit(t2, j4, 2);
-                polyfit_str = ['fitting:' num2str(p(1)) '*x^2+(' num2str(p(2)) '*x)+(' num2str(p(3)) ')'];
+            
+            p=polyfit(t2, j4, 2);
+            polyfit_str = ['fitting:' num2str(p(1)) '*x^2+(' num2str(p(2)) '*x)+(' num2str(p(3)) ')'];
                
-                y1 = polyval(p,t2);
-                figure(2)
-                xlim([100 600])
-                subplot(rowPerFigure,columnPerFigure,ki + (vi-1)*3)
-                plot(t2,j4,'r')
-                ylim([0 150])
-                title([num2str(qV) num2str(qkHz)],'fontsize', 8)
-                %title(['QkHz=' num2str(qkHz) ' QVolts=' num2str(qV)])
-                hold on
-                plot(t2,y1,'b')
-                %legend('Heat Power',polyfit_str) 
-                %ylabel('Heat Power')
-                %xlabel('Inner Core Temp')
-                grid
-                xlim([100 600])
-                hold off
+            y1 = polyval(p,t2);
+            figure(2)
+            xlim([100 600])
+            subplot(rowPerFigure,columnPerFigure,ki + (vi-1)*3)
+            plot(t2,j4,'r')
+            ylim([0 150])
+            title([num2str(qV) num2str(qkHz)],'fontsize', 8)
+            %title(['QkHz=' num2str(qkHz) ' QVolts=' num2str(qV)])
+            hold on
+            plot(t2,y1,'b')
+            %legend('Heat Power',polyfit_str) 
+            %ylabel('Heat Power')
+            %xlabel('Inner Core Temp')
+            grid
+            xlim([100 600])
+            hold off
                
-                tpi = [qV qkHz p(1) p(2) p(3) j4];
-                tp = vertcat(tp,tpi);
-            end
+            tpi = [qV qkHz p(1) p(2) p(3) j4 j5];
+            tp = vertcat(tp,tpi);
         end
     end
 end 
-
-%figure
-%surf(tp(:,1:3));
-
-fn = ['C:\jinwork\BEC\data\tp' num2str(whichEx) '.csv']
+fn = ['C:\jinwork\BEC\tmp\tp' num2str(whichEx) '.csv']
 dlmwrite(fn,tp,',');
